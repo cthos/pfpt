bdbc3c3553d0f2aa8ce5465bca260d747c255526
cthos <daginus@gmail.com>
2012-03-21 17:20:19 -0500

    Adds active effect tracking up to the point where you can see the effects but not the bonuses that have already been applied.

--- src/com/cthos/pfpt/ActiveEffectsActivity.java
+++ src/com/cthos/pfpt/ActiveEffectsActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,14 +17,26 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class ActiveEffectsActivity extends ListActivity
 {
 	protected static final int MENU_ITEM_NEXT_ROUND = 1;
+	protected static final int MENU_ITEM_CLEAR_ALL = 2;
 	
 	protected static final int ADD_EFFECT_DIALOG = 1;
 	
 	protected long characterId;
+	
+	protected SimpleCursorAdapter _adapter;
+	protected Cursor _cursor;
+	
+	private static final String[] PROJECTION = new String[] {
+        "_id",
+        "name",
+        "duration"
+	};
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -31,12 +46,34 @@ public class ActiveEffectsActivity extends ListActivity
 		
 		long charId = getIntent().getLongExtra("characterId", 0);
         characterId = charId;
+        
+        _initButtons();
+        _initList();
 	}
 	
 	public void _initButtons()
 	{
 		Button addButton = (Button) findViewById(R.id.active_effects_add_effect);
 		addButton.setOnClickListener(handleAddClicked);
+	}
+	
+	public void _initList()
+	{	
+		_cursor = managedQuery(
+		Uri.parse("content://com.cthos.pfpt.core.activeeffectprovider/effect"),
+			PROJECTION,
+		 	"character_id = ?",
+		 	new String[]{String.valueOf(characterId)},
+		    "duration DESC"
+	    );
+		
+		startManagingCursor(_cursor);
+		
+		// TODO: Loading data on the UI thread is probably not ideal.
+		_adapter = new SimpleCursorAdapter(this, R.layout.active_effect_list_item, _cursor,
+                new String[] { "name", "duration" }, new int[] {R.id.active_effect_item_name, R.id.active_effect_item_duration});
+        setListAdapter(_adapter);
+        registerForContextMenu(getListView());
 	}
 	
 	@Override
@@ -46,6 +83,9 @@ public class ActiveEffectsActivity extends ListActivity
         menu.add(0, MENU_ITEM_NEXT_ROUND, 0, "Next Round")
                 .setShortcut('1', 'n')
                 .setIcon(android.R.drawable.ic_menu_add);
+        menu.add(0, MENU_ITEM_CLEAR_ALL, 1, "Clear All")
+                .setShortcut('2', 'c')
+                .setIcon(android.R.drawable.ic_menu_delete);
         
         return true;
     }
@@ -59,53 +99,56 @@ public class ActiveEffectsActivity extends ListActivity
 			case MENU_ITEM_NEXT_ROUND:
 				_nextRound();
 	    		break;
+			case MENU_ITEM_CLEAR_ALL:
+				_clearAllEffects();
+				break;
 		}
 		
 		return true;
     }
 	
-	protected Dialog onCreateDialog(int did)
-	{
-		AlertDialog dialog;
-		
-		switch (did) {
-			case ADD_EFFECT_DIALOG:
-			default:
-				dialog = _initAddEffectDialog();
-				break;
-		}
-		
-		return dialog;
-	}
-	
-	protected AlertDialog _initAddEffectDialog()
-	{
-		LayoutInflater inflater = LayoutInflater.from(this);
-		final View custom_view = inflater.inflate(R.layout.add_gear_dialog, null);
-		
-		AlertDialog dialog = new AlertDialog.Builder(this).setTitle("Add Effect")
-            .setView(custom_view)
-            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-				
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();
-				}
-			})
-			.setNegativeButton("Cancel", null).create();
-
-		return dialog;
-	}
-	
 	protected void _nextRound()
 	{
+		ContentResolver cr = getContentResolver();
 		
+	}
+	
+	protected void _clearAllEffects()
+	{
+		ContentResolver cr = getContentResolver();
+		
+		cr.delete(
+			Uri.parse("content://com.cthos.pfpt.core.activeeffectprovider/effect"),
+			"character_id = ?",
+			new String[] {String.valueOf(characterId)}
+		);
+		
+		_refresh();
+	}
+	
+	/**
+	 * Requeries the cursor, notifies the adapter that the data
+	 * set has changed, and then informs the view that it needs to be 
+	 * refreshed.
+	 * 
+	 * @return void
+	 */
+	protected void _refresh()
+	{
+		_cursor.requery();
+		_adapter.notifyDataSetChanged();
+		findViewById(android.R.id.list).invalidate();
 	}
 	
 	private OnClickListener handleAddClicked = new OnClickListener() {
     	public void onClick(View v)
     	{
-    		Activity c = (Activity) v.getContext();
-    		c.showDialog(ADD_EFFECT_DIALOG);
+    		Activity a = (Activity) v.getContext();
+    		
+    		Intent i = new Intent();
+    		i.setClass(a, AddEffectsActivity.class);
+    		i.putExtra("characterId", characterId);
+    		a.startActivity(i);
     	}
     };
 }
